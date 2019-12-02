21b6261a5ac41e2f9c74cc5f8290a71e3faa8846
l1kw1d <my@bunkers.co>
2019-12-02 17:04:18 -0500

    new ws

---
+++ js/new.js
@@ -0,0 +1,11 @@
+// VISUAL Design : Eddie Lobanovskiy
+// https://dribbble.com/shots/6282556-PayJunction-Icons
+
+// WEB ANIMATION & MOTION DESIGN : KONO
+// https://twitter.com/konoanimation
+// http://kono.co.za/
+// hi@kono.co.za
+
+// No JS
+// No plugins
+// 5.97KB size
